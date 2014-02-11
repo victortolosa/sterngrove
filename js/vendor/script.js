183d5ac927a5d880c291052c1e1b2ab6a12f6a63
victorj589 <victorj589@gmail.com>
2014-02-10 19:43:54 -0800

    nav and basic responsiveness

--- js/vendor/script.js
+++ js/vendor/script.js
@@ -1,3 +1,26 @@
-$(window).load(function(){
-
+/* Check Scroll for Nav minify */
+$(window).scroll(function(){
+	var coverHeight = $(".cover").height();
+	var fromTop = $(window).scrollTop();
+	var navLimit = coverHeight - 300;
+	if ( fromTop > navLimit ){
+		$('nav').addClass('navbg');
+		$('.logo').addClass('logo-bp');
+		$('ul.logo-type').css ({width: 300});
+		$('ul.logo-type li').css({
+			"font-size": "26px",
+			"float": "left",
+			"padding-right": "5px",
+			"padding-top": "5px"
+		});
+	} else {
+		$('nav').removeClass('navbg');
+		$('.logo').removeClass('logo-bp');
+		$('ul.logo-type').css ({width: 100});
+		$('ul.logo-type li').css({
+			"font-size": "40px",
+			"padding-right": "5px",
+			"padding-top": "0"
+		});
+	}
 });
