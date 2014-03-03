81a2089c0c7dad0d499eed390e5a8a45a26c1a11
victorj589 <victorj589@gmail.com>
2014-03-02 17:50:45 -0800

    changed splash bg

--- js/production.js
+++ js/production.js
@@ -10,23 +10,9 @@ $(window).scroll(function(){
 	var navLimit = coverHeight - 300;
 	if ( fromTop > navLimit ){
 		$('nav').addClass('navbg');
-		$('.logo').addClass('logo-bp');
-		$('ul.logo-type').css ({width: 300});
-		$('ul.logo-type li').css({
-			"font-size": "26px",
-			"float": "left",
-			"padding-right": "5px",
-			"padding-top": "5px"
-		});
+
 	} else {
 		$('nav').removeClass('navbg');
-		$('.logo').removeClass('logo-bp');
-		$('ul.logo-type').css ({width: 100});
-		$('ul.logo-type li').css({
-			"font-size": "40px",
-			"padding-right": "5px",
-			"padding-top": "0"
-		});
 	}
 });
 
